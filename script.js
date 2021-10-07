a5cf32393cb6526ed587392dd0e34f65651234b7
Oren Dinur <orendinurbs@gmail.com>
2021-10-07 14:33:24 +0300

    Deleted tests

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Data
 const account1 = {
-  owner: 'Oren Dinur',
+  owner: 'Oren/Recruiter Dinur',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
